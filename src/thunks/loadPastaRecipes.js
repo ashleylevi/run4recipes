33540c450ed645493225b9d44e81370a40081882
Ashley Levi <Ashplevi@gmail.com>
2019-01-04 22:54:21 -0700

    Add api key variable into gitignore

--- src/thunks/loadPastaRecipes.js
+++ src/thunks/loadPastaRecipes.js
@@ -1,9 +1,10 @@
 import { loadPastaRecipes } from '../actions/index';
+import { key } from '../apikey';
 
 export const fetchPastaThunk = () => {
   return async (dispatch) => {
     try {
-      const response = await fetch('https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?q=pasta&app_id=0f8e1af8&app_key=a197606b8abb4aeb41aa9f761271a549')
+      const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?q=pasta&app_id=0f8e1af8&app_key=${key}`)
       const recipes = await response.json()
       dispatch(loadPastaRecipes(recipes.hits))     
     } catch(error) {
--- src/thunks/loadPotatoRecipes.js
+++ src/thunks/loadPotatoRecipes.js
@@ -1,9 +1,10 @@
 import { loadPotatoRecipes } from '../actions/index';
+import { key } from '../apikey';
 
 export const fetchPotatoThunk = () => {
   return async (dispatch) => {
     try {
-      const response = await fetch('https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?q=potato&app_id=0f8e1af8&app_key=a197606b8abb4aeb41aa9f761271a549')
+      const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?q=potato&app_id=0f8e1af8&app_key=${key}`)
       const recipes = await response.json()
       dispatch(loadPotatoRecipes(recipes.hits))     
     } catch(error) {
