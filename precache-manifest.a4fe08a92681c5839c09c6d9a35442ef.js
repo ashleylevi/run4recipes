6740a8bc48f5e27af51d2ae02e2aa213a22f60b5
Ashley Levi <Ashplevi@gmail.com>
2019-01-13 15:24:38 -0700

    Updates

---
+++ precache-manifest.a4fe08a92681c5839c09c6d9a35442ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ea574bb5a51c26b260ef",
+    "url": "/static/js/main.ea574bb5.chunk.js"
+  },
+  {
+    "revision": "aaedc92cc668746d280a",
+    "url": "/static/js/1.aaedc92c.chunk.js"
+  },
+  {
+    "revision": "ea574bb5a51c26b260ef",
+    "url": "/static/css/main.3ebf7d84.chunk.css"
+  },
+  {
+    "revision": "d237dc2a49f9d8c27724f83cdb9d4bc6",
+    "url": "/index.html"
+  }
+];
